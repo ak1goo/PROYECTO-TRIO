99f65af7f26ccfa34b70d52ef10887d7f1ca14c7
EstebanRoblero <estebanroblero10@gmail.com>
2025-09-20 17:48:03 -0600

    evaluacion parte uno

---
+++ CODIGO FUENTE.py/evaluacion.py
@@ -0,0 +1,9 @@
+class evaluacion:
+    def __init__(self,codigo, titulo, descripcion=""):
+        self.codigo=codigo
+        self.titulo=titulo
+        self.descripcion=descripcion
+
+    
+    def tupo(self):
+        return "Evaluacion"
---
+++ CODIGO FUENTE.py/reportes.py
@@ -0,0 +1,5 @@
+class curso :
+    def __init__(self, nombre,nota):
+      self.nombre = nombre
+      self.nota=nota
+      
