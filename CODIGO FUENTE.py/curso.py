df71940ca4b014793129173259a1e04307afe705
EstebanRoblero <estebanroblero10@gmail.com>
2025-09-20 18:30:18 -0600

    curso parte dos

--- CODIGO FUENTE.py/curso.py
+++ CODIGO FUENTE.py/curso.py
@@ -17,4 +17,13 @@ class curso :
       self.instructor=instructor
       self.estudiantes={}
       self.evaluaciones={}
-      
+
+    def inscribir_estudiante(self, id_delestudiante, nombre_estudiante):
+       if id_delestudiante in self.estudiantes:
+          raise DuplicateEnrollmentError(
+             f"El estudiante {nombre_estudiante} el ya esta inscrito en {self.codigo}"
+            )
+       self.estudiantes[id_delestudiante]=nombre_estudiante
+       print(f" el estudiante {nombre_estudiante} fue inscrito con el codigo {self.codigo} exitoso")
+
+
