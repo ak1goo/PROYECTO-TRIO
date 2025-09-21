60a5030b64255de4333091271b039e60fa5117d1
EstebanRoblero <estebanroblero10@gmail.com>
2025-09-20 18:10:51 -0600

    evaluacion parte dos

--- CODIGO FUENTE.py/evaluacion.py
+++ CODIGO FUENTE.py/evaluacion.py
@@ -5,5 +5,18 @@ class evaluacion:
         self.descripcion=descripcion
 
     
-    def tupo(self):
+    def tipo(self):
         return "Evaluacion"
+    
+    def __repr__(self):
+        return f"{self.tipo()}({self.codigo}, {self.titulo})"
+
+
+class examen(evaluacion):
+    def tipo(self):
+        return "Examen"
+    
+class tarea(evaluacion):
+    def tipo(self):
+        return "tarea"
+    
