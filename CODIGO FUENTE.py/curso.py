760c5462207ec8e8051464876e503b19e41a8448
EstebanRoblero <estebanroblero10@gmail.com>
2025-09-20 21:14:09 -0600

    curso parte tres

--- CODIGO FUENTE.py/curso.py
+++ CODIGO FUENTE.py/curso.py
@@ -1,13 +1,13 @@
 from evaluacion import evaluacion
 
 class CourseError(Exception):
-   pass
+    pass
 
 class DuplicateEnrollmentError(CourseError):
-   pass
+    pass
 
 class CourseNotFoundError(CourseError):
-   pass
+    pass
 
 
 class curso :
@@ -26,4 +26,24 @@ class curso :
        self.estudiantes[id_delestudiante]=nombre_estudiante
        print(f" el estudiante {nombre_estudiante} fue inscrito con el codigo {self.codigo} exitoso")
 
+    def evaluacion_agregado(self, evaluacion):
+       if evaluacion.codigo in self.evaluaciones:
+          raise CourseError(f" evaluacion {evaluacion.titulo} ya existe en el  curso {self.codigo}")
+               
+       self.evaluaciones[evaluacion.codigo]= evaluacion
+       print(f" evaluaciion{evaluacion.titulo} fue agregado al curso {self.codigo} ")
 
+    def evaluaciones_agregar(self, evaluaciones_listado):
+       for eva in evaluaciones_listado:
+          self.evaluaciones_agregar(eva) 
+
+    def mostrarestudiantes(self):
+       if not self.estudiantes:
+          return "no se encuntra estudiantes inscritos"
+       return [f"{id}-{nombre}" for id, nombre in self.estudiantes.items()] 
+
+    def mostrarevaluaciones(self):
+       if not self.evaluaciones:
+          return "no se encuentra evaluaciones asigandas por el momento"
+       return [f"{eva.tipo()}(eva.codigo), {eva.titulo})" for eva in self.evaluaciones.values()]
+                     
